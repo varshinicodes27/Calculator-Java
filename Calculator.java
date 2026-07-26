d955c65eb4629692a11fbf9d738c6ecb4af3fde1
varshini <varshinipasapula27@gmail.com>
2026-07-26 18:27:13 +0530

    added support for all ops

--- Calculator.java
+++ Calculator.java
@@ -1,25 +1,31 @@
+import CalcLib.MathLib;
 import java.util.*;
 import java.util.stream.Collectors;
-import CalcLib.MathLib;
 
 public class Calculator {
-
-    private static int v1(int a, int b) {
-        MathLib ml = new MathLib();
-        return ml.add(a,b);
-    }
-     private static int v2(int a, int b) {
-        return MathLib.add(a,b);
-    }
-
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         if(input.contains("+")) {
-          List<Integer> arr = Arrays.stream(input.split("[+]")).map(String::trim).map(Integer::parseInt).collect(Collectors.toList());
-            System.out.println(arr);
-            System.out.println(v2(arr.get(0), arr.get(1)));
+            List<Integer> arr = Arrays.stream(input.split("[+]")).map(String::trim).map(Integer::parseInt).collect(Collectors.toList());
+            System.out.println(MathLib.add(arr.get(0), arr.get(1)));
+        }
+        else if(input.contains("-")) {
+            List<Integer> arr = Arrays.stream(input.split("[-]")).map(String::trim).map(Integer::parseInt).collect(Collectors.toList());
+            System.out.println(MathLib.subtract(arr.get(0), arr.get(1)));
+        }
+        else if(input.contains("*")) {
+        List<Integer> arr = Arrays.stream(input.split("[*]")).map(String::trim).map(Integer::parseInt).collect(Collectors.toList());
+
+            System.out.println(MathLib.mul(arr.get(0), arr.get(1)));
+        }
+        else if(input.contains("/")) {
+        List<Integer> arr = Arrays.stream(input.split("[/]")).map(String::trim).map(Integer::parseInt).collect(Collectors.toList());
+
+            System.out.println(MathLib.div(arr.get(0), arr.get(1)));
+        } else {
+            System.out.println("Unsupported operator");
         }
     }
 }
